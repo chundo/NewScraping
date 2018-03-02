4794d14f8dad52c08c024f35975a731b57596957
chundo <segundo.espana1@gmail.com>
2018-03-02 01:07:27 -0500

    Scraping

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get 'noticias' => 'home#noticias'
   get 'scraping' => 'home#scraping'
+  post 'scraping' => 'home#scraping'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
