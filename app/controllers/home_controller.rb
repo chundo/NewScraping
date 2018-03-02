c2abde694ab7b34b16bb5d52c6bc5ebb3a336aac
chundo <segundo.espana1@gmail.com>
2018-03-01 23:39:43 -0500

    Scraping

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  require 'nokogiri'
+  require 'open-uri'
+
   def index
   end
 
@@ -9,18 +12,18 @@ class HomeController < ApplicationController
   def scraping
     
     url = 'http://www.reactiongifs.com/'
-    5.times do |i|
+    1.times do |i|
       puts "Página #{i}"
       document = Nokogiri::HTML(open(url))
       div_main = document.css('div#main')
       div_main.css('div.post').each do |post|
         title = post.css('h2').text
         gif_url = post.css('div.entry img').attr('src')
-        File.open("gifs/#{title}.gif", 'w') do |new_file|
+        File.open(%"./public/imagens/#{title}.gif", 'w') do |new_file|
           puts "Descargando gif: #{title}"
-          open(gif_url, 'r') do |gif|
-            new_file.write(gif.read)
-          end
+          #open(gif_url, 'r') do |gif|
+          #  new_file.write(gif.read)
+          #end
         end
       end
       url = document.css('div.nav-entries div.nav-next a').attr('href')
