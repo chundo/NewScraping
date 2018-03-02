571a7e0bdebfd7e9adcdd68200529aea30b60376
chundo <segundo.espana1@gmail.com>
2018-03-01 23:22:07 -0500

    Scaping and Nikogiri

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails_admin', '~> 1.3'
+gem 'nokogiri'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
-Rails.application.routes.draw do
-  
-  
-  
-  resources :categories
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-    resources :posts
-    
+Rails.application.routes.draw do  
 
   root 'home#index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :categories
+  resources :posts
+
   get 'noticias' => 'home#noticias'
+  get 'scraping' => 'home#scraping'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
